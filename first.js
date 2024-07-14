71266f2ce288edf7af21c6753dc3056c17519fc8
MuskaanDev <muskaanwork24@gmail.com>
2024-07-14 21:53:02 +0530

    comments added

--- first.js
+++ first.js
@@ -1,26 +1,26 @@
-var a = 10
+var a = 10 // var
 
 console.log(typeof a)
 
-let b = "musk"
+let b = "musk" //let
 console.log(typeof b)
 
-const bool  = true
+const bool  = true//boolean
 console.log(typeof bool);
 
-const user ={
+const user ={//object
     username:"musk",
     price:2312
 }
 console.log(typeof user);
 
-const days = ["monday","tuesday"]
+const days = ["monday","tuesday"]//array
 
-console.log(typeof days);
+console.log(typeof days);//object only for arrays also
 
 let f =10
 console.log(f)
 f =11
 console.log(f)
-user={username:"yash"}
+user={username:"yash"} //error because constant assigned
 
