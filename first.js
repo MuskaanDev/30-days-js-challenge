52ce80804226e9a70b4dc1ebf32da7a06f702b91
MuskaanDev <muskaanwork24@gmail.com>
2024-07-14 21:51:18 +0530

    first challenge

---
+++ first.js
@@ -0,0 +1,26 @@
+var a = 10
+
+console.log(typeof a)
+
+let b = "musk"
+console.log(typeof b)
+
+const bool  = true
+console.log(typeof bool);
+
+const user ={
+    username:"musk",
+    price:2312
+}
+console.log(typeof user);
+
+const days = ["monday","tuesday"]
+
+console.log(typeof days);
+
+let f =10
+console.log(f)
+f =11
+console.log(f)
+user={username:"yash"}
+
